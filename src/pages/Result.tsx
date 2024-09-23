a141489c89b293e8ce02c6960e3da4d90bffaa90
한지희, Jihee Han <hatrix1014@gmail.com>
2024-09-24 05:04:26 +0900

    router setup

---
+++ src/pages/Result.tsx
@@ -0,0 +1,7 @@
+export default function Result(){
+  return (
+    <main>
+      <h1>This is ResultPage</h1>
+    </main>
+  )
+}
---
+++ src/pages/Root.tsx
@@ -0,0 +1,7 @@
+export default function Root() {
+    return (
+        <main>
+            <h1>This is Root</h1>
+        </main>
+    );
+}
