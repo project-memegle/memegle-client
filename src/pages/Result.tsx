73919e8516bdd0fc9727425b4dfa7999179c8b5b
한지희, Jihee Han <hatrix1014@gmail.com>
2024-09-25 16:14:09 +0900

    useParams 훅을 사용하여 파라미터값 가져옴

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,7 +1,12 @@
-export default function Result(){
-  return (
-    <main>
-      <h1>This is ResultPage</h1>
-    </main>
-  )
+import { useParams } from 'react-router-dom';
+
+export default function Result() {
+    const params = useParams();
+    const category = params.category;
+
+    return (
+        <main>
+            <h1>This is ResultPage params is {category}</h1>
+        </main>
+    );
 }
