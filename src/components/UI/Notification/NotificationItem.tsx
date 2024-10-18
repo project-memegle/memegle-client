1fe1f95eb4bd096675ad8485b8bd433f8fa01cac
한지희, Jihee Han <hatrix1014@gmail.com>
2024-10-18 23:03:41 +0900

    setup notificaion page css

--- src/components/UI/Notification/NotificationItem.tsx
+++ src/components/UI/Notification/NotificationItem.tsx
@@ -1,8 +1,16 @@
-export default function NotificationItem() {
+type NotificationItemProps = {
+    content: string;
+    date: string;
+};
+
+export default function NotificationItem({
+    content,
+    date,
+}: NotificationItemProps) {
     return (
         <article className="c-notification__item new">
-            <p>이미지 업로드가 수락되었습니다</p>
-            <p className="c-notification__item-date">1일 전</p>
+            <p>{content}</p>
+            <p className="c-notification__item-date">{date}</p>
         </article>
     );
 }
