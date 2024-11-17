5b599714598b25549b4e5d4f9923c7c463d293ad
한지희, Jihee Han <hatrix1014@gmail.com>
2024-11-18 01:02:57 +0900

    Created ChangePassword page

--- src/pages/ChangePassword.tsx
+++ src/pages/ChangePassword.tsx
@@ -1,3 +1,94 @@
+import { AxiosError } from 'axios';
+import ValidationMessages from 'components/Validations/ValidationMessages';
+import useCustomNavigate from 'hooks/useCustomNaviaget';
+import { FormEvent, useCallback, useRef, useState } from 'react';
+import { handleApiError } from 'utils/API/handleApiError';
+import { errorInputCheck } from 'utils/Event/errorInputCheck';
+import passwordCheckHandler from 'utils/SignUp/passwordCheckHandler';
+
 export default function ChangePassword() {
-    return <h1>비밀번호 변경페이지</h1>;
+    const navigate = useCustomNavigate();
+    const [message, setMessage] = useState('');
+    const [password, setPassword] = useState('');
+    const DEFAULT_PASSWORD = ValidationMessages.DEFAULT_PASSWORD;
+    const [passwordCheck, setPasswordCheck] = useState('');
+    const [passwordError, setPasswordError] = useState(DEFAULT_PASSWORD);
+
+    const passwordInputRef = useRef<HTMLInputElement>(null);
+    const passwordCheckInputRef = useRef<HTMLInputElement>(null);
+
+    const onChangePassword = useCallback(
+        passwordCheckHandler(setPassword, passwordCheck, setPasswordError, () =>
+            setMessage('')
+        ),
+        [passwordCheck]
+    );
+
+    const onChangePasswordCheck = useCallback(
+        passwordCheckHandler(setPasswordCheck, password, setPasswordError, () =>
+            setMessage('')
+        ),
+        [password]
+    );
+    const onSubmit = useCallback(
+        async (e: FormEvent<HTMLFormElement>) => {
+            e.preventDefault();
+            if (passwordError) {
+                errorInputCheck(passwordInputRef.current);
+                return;
+            }
+
+            if (password && passwordCheck) {
+                setMessage('');
+                try {
+                    // await signUp(userData);
+                } catch (error) {
+                    handleApiError(error as AxiosError, setMessage);
+                }
+            }
+        },
+        [password, passwordCheck, passwordError]
+    );
+    return (
+        <div className="main__container">
+            <form className="c-login" onSubmit={onSubmit}>
+                <div className="c-login__section">
+                    <p>{passwordError ? passwordError : DEFAULT_PASSWORD}</p>
+                    <section className="c-login__section-password">
+                        <div>
+                            <label htmlFor="password">비밀번호</label>
+                            <input
+                                ref={passwordInputRef}
+                                className="c-login__input"
+                                name="password"
+                                type="password"
+                                id="password"
+                                placeholder="새로운 비밀번호"
+                                value={password}
+                                onChange={onChangePassword}
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="password-check">
+                                비밀번호 확인
+                            </label>
+                            <input
+                                ref={passwordCheckInputRef}
+                                className="c-login__input"
+                                name="password-check"
+                                type="password"
+                                id="password-check"
+                                placeholder="새로운 비밀번호 확인"
+                                value={passwordCheck}
+                                onChange={onChangePasswordCheck}
+                            />
+                        </div>
+                    </section>
+                </div>
+                <button className="button__rounded button__light">
+                    비밀번호 변경하기
+                </button>
+            </form>
+        </div>
+    );
 }
